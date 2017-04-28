44069be936ce145cb0ef42207cb1dfddb454aa60
Krister <krister.torsvik@gmail.com>
2017-04-28 13:30:20 +0200

    styles in widget one will only add to the widget one component

--- src/app/widgets/widget-one.component.ts
+++ src/app/widgets/widget-one.component.ts
@@ -2,8 +2,14 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'widget-one',
+  styles: [`
+    :host{
+      display: block;
+      border: 3px dashed black;
+      font-family: 'Arial';
+    }
+    `],
   template: `
-  <div>one</div>
   <h2>Ones message</h2>
   <h3>{{message}}</h3>
   `
